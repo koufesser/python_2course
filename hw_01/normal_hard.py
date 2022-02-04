8d8ba4b80397addc352f2d8c472021327fe6d254
Fedor Kudriavtsev <koufesser@gmail.com>
2022-02-04 13:21:34 +0300

    added colors

--- hw_01/normal_hard.py
+++ hw_01/normal_hard.py
@@ -10,8 +10,9 @@ cnt = 0
 class NodeVisitor:
     def visit(self, node):
         global cnt
-        node_name = nodename(node)
-        graph.node(str(cnt), node_name)
+        style = nodename(node)
+
+        graph.node(str(cnt), style[0], fillcolor=style[1], shape=style[2], style='filled')
         node_cnt = cnt
         cnt += 1
 
@@ -27,18 +28,28 @@ class NodeVisitor:
 
 def nodename(node):
     name = node.__class__.__name__
+    color = '#DDEDAA'
+    shape = 'rectangle'
     if isinstance(node, ast.Constant):
+        color = '#DDEDAA'
+        shape = 'rectangle'
         name = name + ' ' + str(node.value)
     elif isinstance(node, ast.Name):
+        color = '#F0CF65'
+        shape = 'square'
         name = name + ' ' + str(node.id)
     elif isinstance(node, ast.arg):
+        color = '#D7816A'
+        shape = 'diamond'
         name = name + ' ' + str(node.arg)
     elif isinstance(node, ast.FunctionDef):
+        color = '#BD4F6C'
+        shape = 'octagon'
         name = name + ' ' + str(node.name)
-    return name
+    return (name, color, shape)
 
 
 ast_obj = ast.parse(inspect.getsource(easy.fibonacci))
 n = NodeVisitor()
 n.visit(ast_obj)
-graph.render()
+graph.render(directory='artifacts')
